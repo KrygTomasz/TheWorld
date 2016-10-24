55255b4b42e21f7a2f4b7818c8d0e19ea9a86d10
KrygTomasz <yaami93@gmail.com>
2016-10-24 22:59:44 +0200

    MVC and new .cshtml files
    Added MVC to the project and created three views .cshtml: Index, Contact
    and About. Also added _Layout.cshtml as a main layout of the site so
    other pages can use it to look similar. File named _ViewStart.cshtml
    sets the _Layout.cshtml file as a default layout.
    Added AppController.cs with three methods (each for one existing view).
    Modified Startup.cs to support MVC. Added also enviroment verification
    to show exception page for developers if the unhandled exception
    appears.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -16,13 +16,29 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //app.UseDefaultFiles(); // używane było do wyświetlania pliku .html o domyślnej nazwie index.html
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",// id doesn't have to exist
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
         }
     }
 }
